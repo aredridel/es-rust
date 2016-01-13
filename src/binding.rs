59537250b7913df3cd0a15584fb390703a4160ab
Aria Stewart <aredridel@dinhe.net>
2016-01-12 23:44:22 -0500

    Allow dead code in src/binding

--- src/binding.rs
+++ src/binding.rs
@@ -3,7 +3,10 @@ use list;
 use term;
 
 pub struct Binding {
+#[allow(dead_code)]
 	name: *mut libc::c_char,
+#[allow(dead_code)]
     defn: Box<list::List>,
+#[allow(dead_code)]
     next: Option<Box<Binding>>
 }
