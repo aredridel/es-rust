12318df2fa21f0b10b33566f7553dceaf6b8c519
Aria Stewart <aredridel@dinhe.net>
2016-05-15 22:58:05 -0400

    Remove debug print

--- src/parse.rs
+++ src/parse.rs
@@ -7,7 +7,6 @@ use std::rc::Rc;
 
 impl Es {
     pub fn parse(&self, s: &str) -> Result<List, &'static str> {
-        println!("parse");
         Ok(Cons(Prim("debug".to_string()),
                 Rc::new(Cons(Prim("debug".to_string()),
                              Rc::new(Cons(Str("hello, world!".to_string()), Rc::new(Nil)))))))
